06b9e5185482f546ab1a90941cc00d74c792e7c3
Hector Haro H <hector8.haro@gmail.com>
2022-06-23 12:28:17 -0500

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+
+
+// console.log(nameOfDog)
+// var nameOfDog = 'Fido'
+// console.log(nameOfDog)
+
+nameOfDog()
+var horus = "Horus"
+function nameOfDog() {
+    return `El mejor perro es:  $${horus}`
+}
+
+nameOfDog()
