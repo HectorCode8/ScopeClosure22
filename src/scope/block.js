8300b818961cc4388d08d0d81ddecc2a49ddde32
Hector Haro H <hector8.haro@gmail.com>
2022-06-22 21:16:02 -0500

    const let var

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,9 +3,10 @@ function fruits() {
         var fruit1 = 'apple' // function scope
         let fruit2 = 'kiwi' // let is block scoped
         const fruit3 = 'banana' // const is block scoped
+        console.log(fruit2)
+        console.log(fruit3)
     }
     console.log(fruit1)
-    console.log(fruit2)
-    console.log(fruit3)
+    
 }
 fruits()
---
+++ src/scope/reasignacion.js
@@ -0,0 +1,21 @@
+var firstName // Undefined
+firstName = 'Hector'
+console.log(firstName)
+
+var lastName = 'David' // Declarar / asignar
+lastName = 'Sanchez' // reasignar
+console.log(lastName)
+
+var secondName = 'David' // declarando / asignando
+var secondName = 'Ana' // reasignando
+console.log(secondName)
+
+// Let
+
+let fruit = 'Apple' // declara y asignar
+fruit = 'kiwi' // reasignar
+
+// Const
+
+const animal = 'dog' // declara y asignar
+animal = 'cat' // error
